8d4388095d66c24f3f707decacfa3e88c332c4ab
Abiye <abiyedanagogo@gmail.com>
2020-09-10 20:43:50 +0100

    Started creating the level selector screen

--- core/src/com/abiyedanagogo/game/screens/LevelScreen.java
+++ core/src/com/abiyedanagogo/game/screens/LevelScreen.java
@@ -4,11 +4,52 @@ import com.abiyedanagogo.game.NewGame;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.List;
+import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.utils.Array;
+
 /*
  * Created by Abiye Danagogo on 25/08/2020
  * */
 public class LevelScreen implements Screen {
+
+    private NewGame game;
+    private Stage stage;
+    private Table table;
+    private TextureAtlas atlas;
+    private Skin skin;
+    private List list;
+    private ScrollPane scrollPane;
+    private TextButton play, back;
+
     public LevelScreen(NewGame game) {
+        this.game = game;
+        stage = new Stage();
+        Gdx.input.setInputProcessor(stage);
+
+        atlas = new TextureAtlas("ui/button.pack");
+        skin = new Skin(Gdx.files.internal("ui/menuSkin.json"), atlas);
+
+        table = new Table(skin);
+        table.debug();
+
+        list = new List(skin);
+        //list.setItems(new String[] {"one", "two", "three", "and"});
+        scrollPane = new ScrollPane(list, skin);
+
+        play = new TextButton("PLAY", skin);
+        play.pad(15);
+
+        back = new TextButton("BACK", skin);
+        back.pad(10);
+
+        //Putting table together
+        table.add("SELECT LEVEL");
     }
 
     @Override
@@ -20,6 +61,9 @@ public class LevelScreen implements Screen {
     public void render(float delta) {
         Gdx.gl.glClearColor(0,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        stage.act(delta);
+        stage.draw();
     }
 
     @Override
@@ -44,6 +88,8 @@ public class LevelScreen implements Screen {
 
     @Override
     public void dispose() {
-
+        stage.dispose();
+        atlas.dispose();
+        skin.dispose();
     }
 }
