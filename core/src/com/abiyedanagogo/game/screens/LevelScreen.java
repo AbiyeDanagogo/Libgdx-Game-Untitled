a6ee848a7cd05de79cfef3ce2c773cd49f64f761
Abiye <abiyedanagogo@gmail.com>
2020-09-20 19:59:24 +0100

    Modified LevelScreen.java and menuSkin.json

--- core/src/com/abiyedanagogo/game/screens/LevelScreen.java
+++ core/src/com/abiyedanagogo/game/screens/LevelScreen.java
@@ -1,16 +1,19 @@
 package com.abiyedanagogo.game.screens;
 
 import com.abiyedanagogo.game.NewGame;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.List;
 import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.Array;
 
 /*
@@ -23,11 +26,10 @@ public class LevelScreen implements Screen {
     private Table table;
     private TextureAtlas atlas;
     private Skin skin;
-    private List list;
     private ScrollPane scrollPane;
     private TextButton play, back;
 
-    public LevelScreen(NewGame game) {
+    public LevelScreen(final NewGame game) {
         this.game = game;
         stage = new Stage();
         Gdx.input.setInputProcessor(stage);
@@ -35,29 +37,37 @@ public class LevelScreen implements Screen {
         atlas = new TextureAtlas("ui/atlas.pack");
         skin = new Skin(Gdx.files.internal("ui/menuSkin.json"), atlas);
         skin.getFont("headingfont").getData().setScale(0.2f);
+//        skin.getFont("otherfont").getData().setScale(2f);
 
         table = new Table(skin);
         table.setBounds(0,0,Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         table.debug();
 
-        list = new List(skin);
-        list.setItems(new String[] {"one", "two", "three", "and"});
+        List<String> list = new List<>(skin);
+        String[] myArray = {"one", "two", "thrgddddddddddddddgfggghjjjkjkkkhee", "and", "so", "on", "two", "three", "and", "so", "on", "two", "three", "and", "so", "on", "two", "three", "and", "so", "on", "two", "three", "and", "so", "on"};
+        list.setItems(myArray);
+
         scrollPane = new ScrollPane(list, skin);
 
         play = new TextButton("PLAY", skin);
         play.pad(15);
 
         back = new TextButton("BACK", skin);
+        back.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                ((Game) Gdx.app.getApplicationListener()).setScreen(new MainMenuScreen(game));
+            }
+        });
         back.pad(10);
 
         //Putting table together
         table.add().width(table.getWidth() / 3f);
         table.add("SELECT LEVEL").width(table.getWidth() / 3f);
         table.add().width(table.getWidth() / 3f).row();
-        table.add(scrollPane);
-        table.add(play).size(play.getWidth()/ 12f, play.getHeight()/ 12f);
-        table.add(back).size(back.getWidth()/ 12f, back.getHeight()/ 12f);
-
+        table.add(scrollPane).expandY();
+        table.add(play).size(play.getWidth()/ 5f, play.getHeight()/ 5f);
+        table.add(back).size(back.getWidth()/ 5f, back.getHeight()/ 5f);
         stage.addActor(table);
     }
 
